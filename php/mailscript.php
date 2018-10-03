376932a004dda7f1e4781124d2c2f23d26de9ba0
Hutson Olsen <hutson.olsen@gmail.com>
2018-10-02 19:27:18 -0500

    Restructure

--- php/mailscript.php
+++ php/mailscript.php
@@ -2,13 +2,13 @@
 
 if(isset($_POST['submit'])) {
 
-	$to = 'hutson@hutsondolsen.com'; // PUT YOUR EMAIL ADRESS INSTEAD your.website@site.com
+	$to = 'your.website@site.com'; // PUT YOUR EMAIL ADRESS INSTEAD your.website@site.com
 	$subject = $_POST['subject'];
 	$message = '<b>Email from your website.</b><br><br>
 				First name: '.$_POST['first_name'].'<br>'.'
 				Last name: '.$_POST['last_name'].'<br>'.'
 				Email: '.$_POST['email'].'<br>'.'
-				Message: '.$_POST['message'].'<br>';
+				Message: '.$_POST['massage'].'<br>';
 	$mheaders  = "Content-type: text/html; charset=utf-8 \r\n"; 
 	$mheaders .= "From: Your website <your.website@site.com>\r\n"; 
 	mail($to, $subject, $message, $mheaders);
